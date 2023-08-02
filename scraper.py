669e2f0fb78d5e9cd15c4f93bbb866e5e809b167
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-08-02 19:24:58 -0400

    Added unit info

--- scraper.py
+++ scraper.py
@@ -9,4 +9,16 @@ request_page.close()
 
 html_soup = BeautifulSoup(page_html, 'html.parser')
 
-house_price = html_soup.find_all('div', class_ = "priceWrapper-1165431705" )
+house_title = html_soup.find_all('div', class_ = "realEstateTitle-1440881021" )
+
+for title in house_title:
+    title = title.find('div', class_ ="titleRow-4059548442").text
+    price = title.find('div', class_ = "priceWrapper-1165431705").text
+
+house_unit = html_soup.find_all('div', class_ = "unitRow-1281171205")
+
+for unit in house_unit:
+    style = unit.find('li', class_ = "noLabelAttribute-2328647506").text
+    bedrooms = unit.find('span', class_ = "noLabelValue-3861810455").text
+    bathrooms = unit.find('span', class_ = "noLabelValue-3861810455").text
+
