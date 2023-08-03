9f471bc994fbcd0a3af9d3daab2e19f646786fea
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-08-02 20:10:00 -0400

    Attempted scraping

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url_to_scrape = "https://www.kijiji.ca/b-kitchener-waterloo/apartment-rent-in-kitchener/k0l1700212"
+url_to_scrape = "https://www.kijiji.ca/v-apartments-condos/kitchener-waterloo/luxury-1-bedroom-apartment-for-rent/1658473493"
 
 request_page = urlopen(url_to_scrape)
 page_html = request_page.read()
@@ -9,16 +9,29 @@ request_page.close()
 
 html_soup = BeautifulSoup(page_html, 'html.parser')
 
-house_title = html_soup.find_all('div', class_ = "realEstateTitle-1440881021" )
+house_title = html_soup.find_all('div', class_="realEstateTitle-1440881021" )
 
-for title in house_title:
-    title = title.find('div', class_ ="titleRow-4059548442").text
-    price = title.find('div', class_ = "priceWrapper-1165431705").text
+filename = 'products.csv'
+f = open(filename, 'w')
 
+headers = 'Title, Price \n'
+
+f.write(headers)
+
+for main in house_title:
+    title = main.find('h1', class_="title-2323565163").text
+    price = main.find('div', class_= "priceWrapper-1165431705").text
+
+    f.write(title + ', ' + price)
+
+f.close()
+
+'''
 house_unit = html_soup.find_all('div', class_ = "unitRow-1281171205")
+
 
 for unit in house_unit:
     style = unit.find('li', class_ = "noLabelAttribute-2328647506").text
     bedrooms = unit.find('span', class_ = "noLabelValue-3861810455").text
     bathrooms = unit.find('span', class_ = "noLabelValue-3861810455").text
-
+'''
