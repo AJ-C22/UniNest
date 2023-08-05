26da5d123df6e7247f845673b76e8a854d362abf
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-08-04 21:52:13 -0400

    Attempted to fix scraper

--- scraper.py
+++ scraper.py
@@ -1,18 +1,56 @@
 #Figure out how to scrape into other pages 
 #Dyanmic scraping w selenium
-import urllib.request
-from urllib.request import urlopen
+#import urllib.request
+#from urllib.request import urlopen
 from bs4 import BeautifulSoup
+#from selenium import webdriver
+#from selenium.webdriver import Chrome
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+import requests
+from requests_html import HTMLSession
 
-url_to_scrape = "https://www.kijiji.ca/b-apartments-condos/kitchener-waterloo/c37l1700212"
+url_to_scrape = "https://www.kijiji.ca/b-kitchener-waterloo/apartment-rent-in-kitchener/k0l1700212"
 
+'''
+driver = webdriver.Chrome()
+driver.get(url_to_scrape)
+'''
+
+session = HTMLSession()
+response = session.get(url_to_scrape)
+response.html.render()
+
+print(response.html)
+print(response.html.find('div.sc-df900376 gsWCmx'))
+#soup = BeautifulSoup(response.content, 'html.parser')
+
+#house_title = soup.find_all('div', class_ ="sc-df900376.gsWCmx")
+
+#print(house_title)
+
+
+
+
+
+
+'''
 request_page = urlopen(url_to_scrape)
 page_html = request_page.read()
 request_page.close()
-
 html_soup = BeautifulSoup(page_html, 'html.parser')
 
-house_title = html_soup.find_all('div', class_ ="sc-df900376.gsWCmx")
+listings = driver.find_elements(By.NAME('sc-df900376 gsWCmx'))
+
+for listing in listings:
+    title = listing.find_element(By.xpath('.//*[@id="__next"]/div/div/main/div[2]/div[3]/div[3]/div[3]/div[1]/ul/li[1]/section/div[1]/div[2]/div[1]')).text
+    price = listing.find_element(By.xpath('.//*[@id="__next"]/div/div/main/div[2]/div[3]/div[3]/div[3]/div[1]/ul/li[1]/section/div[1]/div[2]/div[1]/h3/a')).text
+    print(title,price)
+
+driver.quit()
+'''
+'''
+house_title = html_soup.find_all('div', class_ ="sc-df900376 gsWCmx")
 print(house_title)
 filename = 'products.csv'
 f = open(filename, 'w')
@@ -30,13 +68,4 @@ for main in house_title:
 
 f.close()
 print(page_html)
-
-'''
-house_unit = html_soup.find_all('div', class_ = "unitRow-1281171205")
-
-
-for unit in house_unit:
-    style = unit.find('li', class_ = "noLabelAttribute-2328647506").text
-    bedrooms = unit.find('span', class_ = "noLabelValue-3861810455").text
-    bathrooms = unit.find('span', class_ = "noLabelValue-3861810455").text
 '''
