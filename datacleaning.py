b1587d900e02de809ee58a2caf74b33ab5f85932
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-08-08 16:25:53 -0400

    Updated datacleaner

--- datacleaning.py
+++ datacleaning.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('products.csv', encoding="ISO-8859-1")
 
@@ -7,6 +8,7 @@ df = pd.read_csv('products.csv', encoding="ISO-8859-1")
 df = df[df["Title"].str.contains("Wanted:") == False]
 
 #Bedroom number only
+df[" Bedrooms"] = df[" Bedrooms"].str.replace('Bedrooms: ', '')
 
 #Bathroom number only
 df[" Bathrooms"] = df[" Bathrooms"].str.replace('Bathrooms: ', '')
@@ -15,5 +17,6 @@ df[" Bathrooms"] = df[" Bathrooms"].str.replace('Bathrooms: ', '')
 df[" Size"] = df[" Size"].str.replace('Not Available', 'N/A')
 
 #Parse bedrooms (den, studio)
-den_val = df[" Bedrooms"].str.find("Den")
-
+df['Den'] = np.where(df[" Bedrooms"].str.find("Den") > 0, "Yes", "No")
+df[" Bedrooms"] = df[" Bedrooms"].str.replace('+ Den ', '')
+print(df)
