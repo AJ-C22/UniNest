27abf749d170d13295d7d1024cbc044978d16937
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-08-10 15:06:39 -0400

    Added to data cleaning

--- datacleaning.py
+++ datacleaning.py
@@ -2,14 +2,8 @@ import pandas as pd
 import numpy as np
 import csv
 
-filename = 'finalfile.csv'
-f = open(filename, 'w')
-f.write('Location,Title,Price,Style,Bedrooms,Bathrooms,Size,Air Conditioned  \n')
-f.close()
 
-
-'''
-df = pd.read_csv('products.csv', encoding="ISO-8859-1")
+df = pd.read_csv('finalfile.csv', encoding="ISO-8859-1")
 
 
 #Remove "Wanteds"
@@ -26,5 +20,11 @@ df["Size"] = df["Size"].str.replace('Not Available', 'N/A')
 
 #Parse bedrooms (den, studio)
 df['Den'] = np.where(df["Bedrooms"].str.find("Den") > 0, "Yes", "No")
-df["Bedrooms"] = df["Bedrooms"].str.replace('+ Den ', '')
-'''
+df["Bedrooms"] = df["Bedrooms"].str.replace('+ Den', '')
+
+#Remove extra headers
+df = df[df["Title"].str.contains("Title") == False]
+print(df)
+
+#Remove incorrectly scraped data
+df = df[df["Style"].str.contains("No") == False]
