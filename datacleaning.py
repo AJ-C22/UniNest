2823bab0a03f9dce11bfe2701b1695e5082e3e42
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-08-17 12:59:35 -0400

    STarted making graphs + cleaned data

--- datacleaning.py
+++ datacleaning.py
@@ -11,9 +11,11 @@ df = df[df["Title"].str.contains("Wanted:") == False]
 
 #Bedroom number only
 df["Bedrooms"] = df["Bedrooms"].str.replace('Bedrooms: ', '')
+df = df[df["Bedrooms"].str.contains("N/A") == False]
 
 #Bathroom number only
 df["Bathrooms"] = df["Bathrooms"].str.replace('Bathrooms: ', '')
+df = df[df["Bathrooms"].str.contains("N/A") == False]
 
 #Turn "Not Available" to "N/A"
 df["Size(sqft)"] = df["Size(sqft)"].str.replace('Not Available', 'N/A')
@@ -22,19 +24,23 @@ df["Size(sqft)"] = df["Size(sqft)"].str.replace('Not Available', 'N/A')
 df['Den'] = np.where(df["Bedrooms"].str.find("Den") > 0, "Yes", "No")
 df["Bedrooms"] = df["Bedrooms"].str.replace('+ Den', '')
 
+df['Bachelor/Studio'] = np.where(df["Bedrooms"].str.find("Bachelor/Studio") > 0, "Yes", "No")
+df["Bedrooms"] = df["Bedrooms"].str.replace('Bachelor/Studio', '0')
+
 #Remove extra headers
 df = df[df["Title"].str.contains("Title") == False]
-print(df)
 
 #Remove incorrectly scraped data
 df = df[df["Style"].str.contains("No") == False]
 
 #Remove dollar sign
-df["Price($)"] = df["Price($)"].str.replace('$', '')
-df["Price($)"] = df["Price($)"].str.replace('.00', '')
+df["Price"] = df["Price"].str.replace('$', '')
+df["Price"] = df["Price"].str.replace('.00', '')
 
 #Remove "Please Contact" prices
-df = df[df["Price($)"].str.contains("Please Contact") == False]
+df = df[df["Price"].str.contains("Please Contact") == False]
+df = df = df[df["Title"].str.contains("Please Contact") == False]
+
 
 #Turn df into csv
 df.to_csv("housing_cleaned.csv")
