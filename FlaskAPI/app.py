ab7c52e4dc2e352d82e1e4b540b8fced73fd6c6a
AJ-C22 <114104270+AJ-C22@users.noreply.github.com>
2023-12-17 20:31:17 -0500

    Finished FlaskAPI

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,13 +1,32 @@
 import flask
 from flask import Flask, jsonify, request
 import json
+import pickle
+import numpy as np
+
+data_in = [1.0, 1.0, 540.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+
+def load_models():
+    file_name = "models/model_file.p"
+    with open(file_name, 'rb') as pickled:
+        data = pickle.load(pickled)
+        model = data['model']
+        return model
 
 app = Flask(__name__)
 @app.route('/predict', methods=['GET'])
-
 def predict():
-    response = json.dumps({'response': 'yahhhh!'})
+    request_json = request.get_json()
+    x = request_json['input']
+    
+    x_in = np.array(x).reshape(1,-1)
+    print(x_in)
+    model = load_models()
+    prediction = model.predict(x_in)[0]
+    response = json.dumps({'response': prediction})
     return response, 200
 
 if __name__ == '__main__':
     application.run(debug=True)
+
